446bcb6b146b0d27fb18b7d8557d2288fc649729
Ezeomeke ozioma <christ.stephaug@gmail.com>
2023-04-29 16:12:43 -0400

    secondproject

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,8 +16,10 @@ urlpatterns = [
      path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
 
